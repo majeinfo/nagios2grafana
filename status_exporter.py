7de4d4f2a3a5eacfea96f572a031a08e9f9ea0a0
Jerome Delamarche <jd@maje@biz>
2020-11-14 12:17:34 +0100

    Fix plugin_output value to avoid Grafana bug ?

--- status_exporter.py
+++ status_exporter.py
@@ -23,11 +23,12 @@ svc_data = [{
     "rows": []
 }]
 
+# Make sure the plugin_output does not start with a number (Grafana bug ?)
 def build_host_data(host_status):
     host_data[0]['rows'] = []
     for h in host_status:
         host_data[0]['rows'].append(
-            [h['current_state'], h['host_name'], h['last_check'], h['plugin_output']]
+            [h['current_state'], h['host_name'], h['last_check'], '>' + h['plugin_output']]
         )
 
     return host_data
@@ -37,7 +38,7 @@ def build_svc_data(svc_status):
     svc_data[0]['rows'] = []
     for s in svc_status:
         svc_data[0]['rows'].append(
-            [s['current_state'], s['host_name'], s['service_description'], s['last_check'], s['plugin_output']]
+            [s['current_state'], s['host_name'], s['service_description'], s['last_check'], '>' + s['plugin_output']]
         )
 
     return svc_data
